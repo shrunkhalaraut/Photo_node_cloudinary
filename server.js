1be46c2dcf04f2b965ec5276d0a3ea20f8a2c7f6
shrunkhalaraut <shrunkhalaraut@gmail.com>
2023-05-31 18:46:03 +0530

    ejs added

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const dotEnv= require("dotenv");
 dotEnv.config();
+const multer  = require('multer');
+const uploads = multer({ dest: "files/"});
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true}));
+
+app.get("/", (req,res) =>{
+    res.render("display");
+});
+
+app.get("/add", (req,res) =>{
+    res.render("add");
+})
+
+app.post("/add",(req,res)=>{
+    console.log("post here")
+})
 
 app.listen(5000, ()=> console.log("Server Started"));
