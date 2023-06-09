ae68b1123ec3a233bc425714f414dc15588321e7
shrunkhalaraut <shrunkhalaraut@gmail.com>
2023-06-09 22:19:44 +0530

    model commit

--- models/image.js
+++ models/image.js
@@ -1,13 +1,14 @@
 const mongoose= require('mongoose');
 
-const imageSchema = new mongoose.Schema({
+const imageSchema = mongoose.Schema({
     name: {
         type:String,
         required: true,
     },
     image: {
-        url: {type: String,
-        required: true,
+        url: {
+            type: String,
+            required: true,
         },
         filename: {
             type: String,
--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const dotEnv= require("dotenv");
 dotEnv.config();
 const multer  = require('multer');
 const {storage}= require('./cloudinary/index');
-//const uploads = multer({ dest: "files/"});
+const uploads = multer({ dest: "files/"});
+
 //cloud storage
-const uploads = multer({ storage });
-const mongoose = require("mongoose")
-
-
+//const uploads = multer({ storage });
+const mongoose = require("mongoose");
+const Image = require("./models/image");
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true}));
 //app.set('views', path.join(__dirname, '/views'))
 app.use(express.json())
 
-const url= "mongodb+srv://shrunkhalaraut:D9QoQLb9zGiVi7sJ@cluster0.dwabuzw.mongodb.net/photonodedb"
+//const url= "mongodb+srv://shrunkhalaraut:D9QoQLb9zGiVi7sJ@cluster0.dwabuzw.mongodb.net/photonodedb"
 
-const connectionParams = {
-    useNewUrlParser: true,    
-    useUnifiedTopology: true    
-    }
+//const connectionParams = {
+ //   useNewUrlParser: true,    
+ //   useUnifiedTopology: true    
+//    }
 
 mongoose.connect(url, connectionParams).then(() => {    
     console.log('connected to db')    
@@ -39,9 +39,13 @@ app.get("/add", (req,res) =>{
 })
 
 app.post("/add",uploads.single("image"),(req,res)=>{
-
-
    // console.log("post here");
+   const newImage = new Image(req.body)
+   newImage.image.url = req.file.path;
+   newImage.image.filename = req.file.filename;
+   console.log(newImage);
+   await newImage.save();
+   res.redirect();
     console.log(req.body, req.file);
 });
 
